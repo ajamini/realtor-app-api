850869fce5d7adda08426dd104e1a815f19a0676
iraj <iraj.amini@outlook.com>
2022-10-19 12:26:25 -0400

    remove .gitignore

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from spaces import Client
 import time, pdf
 
@@ -13,26 +13,28 @@ client = Client(
 
 
 
-@app.route("/generate")
-def generate():
+@app.route("/generate/<form_number>", methods=['POST'])
+def generate(form_number):
+    body = request.get_json()
     current_time = int(time.time()*1000)
-    pdf_template = "form/form-801.pdf"
-    pdf_output = f"{current_time}.pdf"
-    print(pdf_output)
+
+    pdf_template = f"form/form-{form_number}.pdf"
+    pdf_output = f"form-{form_number}-{current_time}.pdf"
 
     client.download_file('form/form-801.pdf', destination='')
-
-    sample_data_dict = {
-        '100-1': 'adf',
-        '100-2': 'fhgh',
-        '100-3': 'hjbn',
-        '100-4': 'cbncbncbncbn',
-        '100-5': 'jhjhsrsf sdf',
-        '100-6': '454',
-        '100-7': '676',
-        '100-8': '5656',
-    }
-    pdf.fill(pdf_template, pdf_output, sample_data_dict)
-    client.upload_file(file=pdf_output, destination='output/')
+    #
+    # print(body);
+    # sample_data_dict = {
+    #     '100-1': 'adf',
+    #     '100-2': 'fhgh',
+    #     '100-3': 'hjbn',
+    #     '100-4': 'cbncbncbncbn',
+    #     '100-5': 'jhjhsrsf sdf',
+    #     '100-6': '454',
+    #     '100-7': '676',
+    #     '100-8': '5656',
+    # }
+    pdf.fill(pdf_template, pdf_output, body)
+    client.upload_file(pdf_output, 'output/')
 
     return {"body": pdf_output}
